64fab3701305d591344c437f680f26cee4864e39
Jason S. Jones <jsjones96@gmail.com>
2015-01-25 14:02:57 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Priority Queue Data Structure
+
+## Description
+
+This is a javascript implementation of a
+[priority queue](http://en.wikipedia.org/wiki/Priority_queue)
+data structure.
---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * @fileOverview Implementation of a priority queue data structure
+ * @author Jason S. Jones
+ * @version 0.1.0
+ * @license MIT
+ */
+
+(function() {
+    'use strict';
+
+    var PriorityQueue = function() {
+
+    };
+
+    module.exports = PriorityQueue;
+})();
